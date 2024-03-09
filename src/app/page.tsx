bc69003b12adb36a4db1d79e1b5adb127abff837
Yura <svin07@ukr.net>
2024-03-09 22:38:44 +0200

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import ActiveLabel from './components/active-label';
-import NotActiveLabel from './components/not-active-label';
+import { Stream } from 'stream';
+import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
     <main>
-      <h1>My Next</h1>
-      <ActiveLabel>Active</ActiveLabel>
-      <NotActiveLabel>Not active</NotActiveLabel>
+      <h1 className="text-lime-800 underline text-xl m-4 italic font-bold">
+        My Next
+      </h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Panding}>Panding</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
